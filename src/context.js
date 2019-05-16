e861b3721f19c1ae31225792cc2ce0b1f94accd8
jermaine <jermainehaughton78@yahoo.com>
2019-05-16 12:57:06 -0400

    finish setting up cart and added 404 error page

--- src/context.js
+++ src/context.js
@@ -12,9 +12,88 @@ class ProductProvider extends Component {
         products: [],
         detailProduct: detailProduct,
         cart: [],
-        modalOpen: true,
+        modalOpen: false,
         modalProduct: detailProduct,
+        cartSubTotal: 0,
+        cartTax: 0,
+        cartTotal: 0
+
     };
+increment = (id)=>{
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item=>item.id === id);
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+    product.count = product.count + 1;
+    product.total = product.count * product.price;
+
+    this.setState(()=>{
+        return {
+            cart: [...tempCart]
+        }
+    },()=>{this.addTotals()})
+}
+
+decrement = (id)=>{
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item=>item.id === id);
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+    product.count = product.count - 1;
+    if(product.count === 0){
+        this.removeItem(id)
+    } else {
+    product.total = product.count * product.price;
+
+    this.setState(()=>{
+        return {
+            cart: [...tempCart]
+        }
+    },()=>{this.addTotals()})}
+}
+
+removeItem = (id)=>{
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
+    tempCart = tempCart.filter(item => item.id !== id);
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removedProduct = tempProducts[index];
+    removedProduct.inCart = false;
+    removedProduct.count = 0;
+    removedProduct.total = 0;
+
+    this.setState(()=>{
+        return{
+            cart: [...tempCart],
+            products: [...tempProducts],
+        }
+    },()=>{
+        this.addTotals();
+    })
+}
+
+clearCart = ()=>{
+    this.setState(()=>{
+        return {cart: []};
+    }, ()=> {
+        this.setProducts();
+    })
+}
+
+addTotals = ()=>{
+    let subTotal = 0;
+    this.state.cart.map(item=>(subTotal += item.total));
+    const tempTax = subTotal * 0.1;
+    const tax = parseFloat(tempTax.toFixed(2));
+    const total = subTotal + tax;
+    this.setState(()=>{
+        return {
+            cartSubtotal: subTotal,
+            cartTax: tax,
+            cartTotal: total
+        }
+    })
+}
     setProducts = () => {
         let tempProducts = [];                                     // loops through the store products list and gets all the values
         storeProducts.forEach(item => {                             // making sure to get a copy of the values and not a actual
@@ -51,7 +130,7 @@ class ProductProvider extends Component {
         product.total = price;
         this.setState(()=>{
             return {products: tempProducts, cart:[...this.state.cart, product]}
-        }, ()=> console.log(this.state.cart)); // for debuging to make sure item is added to cart
+        },()=>this.addTotals(), ()=> console.log(this.state.cart)); // for debuging to make sure item is added to cart
    
     }
 
@@ -69,7 +148,18 @@ class ProductProvider extends Component {
     }
     render() {
         return ( <ProductContext.Provider value = {
-                {...this.state, handleDetail: this.handleDetail, addToCart: this.addToCart, openModal: this.openModal, closeModal: this.closeModal} } > { this.props.children } 
+                {...this.state, 
+                handleDetail: this.handleDetail, 
+                addToCart: this.addToCart, 
+                openModal: this.openModal, 
+                closeModal: this.closeModal,
+                increment: this.increment,
+                decrement: this.decrement,
+                removeItem: this.removeItem,
+                clearCart: this.clearCart,
+                
+                }} > 
+                { this.props.children } 
                 </ProductContext.Provider>
         );
     }
